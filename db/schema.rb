44868df8e53269aab68c5454697b0255827b4ff2
Jason Shultz <jasshultz@gmail.com>
2013-05-18 17:11:37 -0600

    fixed adding user id to projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130518145449) do
+ActiveRecord::Schema.define(:version => 20130518231033) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -31,8 +31,14 @@ ActiveRecord::Schema.define(:version => 20130518145449) do
   add_index "admins", ["email"], :name => "index_admins_on_email", :unique => true
   add_index "admins", ["reset_password_token"], :name => "index_admins_on_reset_password_token", :unique => true
 
-# Could not dump table "projects" because of following StandardError
-#   Unknown type 'email' for column 'user_id'
+  create_table "projects", :force => true do |t|
+    t.string   "name"
+    t.string   "title"
+    t.text     "details"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+  end
 
   create_table "tags", :force => true do |t|
     t.string   "name"
